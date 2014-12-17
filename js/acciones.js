6d06b0930f80c7633abe5ecc32b04631f7ace093
AL02F12 <fede1ctuv@live.com>
2014-12-17 09:36:50 -0600

    nueve

--- js/acciones.js
+++ js/acciones.js
@@ -4,18 +4,18 @@ function buscarlibros ()
 		type:"POST",
 		url: "http://192.168.1.188/practica12/listado_general.php"
 	}).done(function(msg) {
-		alert (msg);
+		
 		var DatosJSON=JSON.parse(msg);
 		
 		if (DatosJSON.datos==1)
 		{
-			alert ('datos');
+			
 			$('#libros').empty();
 			for (var i=0; i <DatosJSON.libros.length; i++){
 					
 			$('#libros').append('<div style="float:left;width:45%"> <p>Libro: '+DatosJSON.libros[i].Nombre_libro+' <br> Autor :'+DatosJSON.libros[i].Autor+' </p> </div> <div style="float:left;width:55%"> <img class="portada" src="http://192.168.1.188/practica12/recursos/fotos/'+DatosJSON.libros[i].Id+'.jpg">  </div> <div style="clear:both">  <hr> <hr> </div>');
 			}
-			alert ('datos 2');
+			
 			$("#page2").trigger('pagecreate');
 		}
 		
@@ -37,7 +37,7 @@ $(document).ready(function(e) {
  document.addEventListener("deviceready",function(){
   $('#todos').tap(function(){
     buscarlibros ();
-	alert ("fuera");
+
 	$.mobile.changePage('#page2');
   });
  }); //deviceready
