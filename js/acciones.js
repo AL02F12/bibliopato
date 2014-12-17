17ba020b2425095955d16ece0d023775c3a6ba44
AL02F12 <fede1ctuv@live.com>
2014-12-17 10:26:12 -0600

    diez

--- js/acciones.js
+++ js/acciones.js
@@ -4,35 +4,50 @@ function buscarlibros ()
 		type:"POST",
 		url: "http://192.168.1.188/practica12/listado_general.php"
 	}).done(function(msg) {
-		
 		var DatosJSON=JSON.parse(msg);
-		
 		if (DatosJSON.datos==1)
 		{
-			
 			$('#libros').empty();
 			for (var i=0; i <DatosJSON.libros.length; i++){
 					
 			$('#libros').append('<div style="float:left;width:45%"> <p>Libro: '+DatosJSON.libros[i].Nombre_libro+' <br> Autor :'+DatosJSON.libros[i].Autor+' </p> </div> <div style="float:left;width:55%"> <img class="portada" src="http://192.168.1.188/practica12/recursos/fotos/'+DatosJSON.libros[i].Id+'.jpg">  </div> <div style="clear:both">  <hr> <hr> </div>');
 			}
-			
 			$("#page2").trigger('pagecreate');
-		}
-		
+			}
 		if (DatosJSON.datos==0)
 		{
 			alert("No hay Libros que mostrar");
 		}
 		$('#page2').trigger('pagecreate');
 	});
-	
-	
 }
 
+  
+function buscargenero ()
+{
+	$.ajax({
+		type:"POST",
+		url: "http://192.168.1.188/practica12/ConsultaGenero.php"
+	}).done(function(msg) {
+		var DatosJSON=JSON.parse(msg);
+		if (DatosJSON.datos==1)
+		{
+			$('#libros').empty();
+			for (var i=0; i <DatosJSON.libros.length; i++){
+					
+			$('#libros').append('<div style="float:left;width:45%"> <p> Género:: '+DatosJSON.libros[i].Genero+' <br> Libro: '+DatosJSON.libros[i].Nombre_libro+' <br> Autor :'+DatosJSON.libros[i].Autor+' </p> </div> <div style="float:left;width:55%"> <img class="portada" src="http://192.168.1.188/practica12/recursos/fotos/'+DatosJSON.libros[i].Id+'.jpg">  </div> <div style="clear:both">  <hr> <hr> </div>');
+			}
+			$("#page4").trigger('pagecreate');
+			}
+		if (DatosJSON.datos==0)
+		{
+			alert("No hay Géneros que mostrar");
+		}
+		$('#page4').trigger('pagecreate');
+	});
+}
 
-
-
-
+   
 $(document).ready(function(e) {
  document.addEventListener("deviceready",function(){
   $('#todos').tap(function(){
@@ -40,6 +55,12 @@ $(document).ready(function(e) {
 
 	$.mobile.changePage('#page2');
   });
- }); //deviceready
-}); //document ready
+  
+ $('#todos').tap(function(){
+    buscargenero ();
 
+	$.mobile.changePage('#page4');
+  }); 
+ });  
+ }); 
+ 
